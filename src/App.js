8b42870eec49c6fef7763c4f600e84dc7c81a52f
andreLagacione <lagacioneandre@gmail.com>
2020-02-10 18:28:48 -0300

    create styles for application wrapper and add top component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.scss';
 
 import Sidebar from './components/sidebar/sidebar';
+import Top from './components/top/top'
 
 function App() {
   return (
-    <>
+    <div id="application">
       <div id="sidebar">
         <Sidebar />
       </div>
 
-      <main id="content-app"></main>
-    </>
+      <main id="content-app">
+        <Top />
+      </main>
+    </div>
   );
 }
 
