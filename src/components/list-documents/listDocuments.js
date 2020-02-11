059acbdbd278474f6c70c152a633fdeaae6d5129
andreLagacione <lagacioneandre@gmail.com>
2020-02-10 22:50:48 -0300

    create service to get documents in back end

--- src/components/list-documents/listDocuments.js
+++ src/components/list-documents/listDocuments.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
 function ListDocuments() {
+
+    const [documents, setDocuments] = useState([]);
+
+    useEffect(() => {
+        async function loadDocuments() {
+            const response1 = await api.get('/document');
+            const response2 = await api.get('/edited-document');
+            let documentList = [];
+
+            Promise.all([response1, response2]).then((_response) => {
+                _response.forEach(item => {
+                    item.data.map(subItem => {
+                        documentList.push(subItem);
+                    });
+                });
+
+                console.log(documentList);
+                setDocuments(documentList);
+            });
+        }
+
+        loadDocuments();
+    }, []);
 
 
     return (
@@ -19,56 +43,20 @@ function ListDocuments() {
             </thead>
 
             <tbody>
-                <tr>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                </tr>
-                <tr>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                </tr>
-                <tr>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                </tr>
-                <tr>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                </tr>
-                <tr>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                </tr>
+                {
+                    documents.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.cnpjEmissor}</td>
+                            <td>{item.cidadeEstadoEmissor}</td>
+                            <td>{item.cnpjRemetente}</td>
+                            <td>{item.cidadeEstadoRemetente}</td>
+                            <td>{item.cnpjDestinatario}</td>
+                            <td>{item.cidadeEstadoDestinatario}</td>
+                            <td>{item.chaveAcesso}</td>
+                            <td>{item.dataEmissao}</td>
+                        </tr>
+                    ))
+                }
             </tbody>
         </table>
     );
