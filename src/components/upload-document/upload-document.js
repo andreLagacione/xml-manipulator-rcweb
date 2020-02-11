b617878db8d9c13ca27fd0a3109418dea285343f
André Lagacione <lagacioneandre@gmail.com>
2020-02-11 16:55:28 -0300

    create form to update file

--- src/components/upload-document/upload-document.js
+++ src/components/upload-document/upload-document.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 function UploadDocument() {
-    return(
-        <h1>Upload de documentos!!</h1>
+    return (
+        <>
+            <form onSubmit="" id="document-upload">
+                <input type="file" name="fileInput" id="fileInput" />
+                <label htmlFor="fileInput" className="file-name">
+                    <span className="btn-select">Selecione</span>
+                </label>
+
+                <div className="row-button">
+                    <button className="btn-submit">
+                        <FontAwesomeIcon icon={faUpload} /> Enviar
+                </button>
+                </div>
+            </form>
+
+            <div className="xml-document">
+                <h3 className="title">Documento enviado</h3>
+            </div>
+        </>
     );
 }
 
