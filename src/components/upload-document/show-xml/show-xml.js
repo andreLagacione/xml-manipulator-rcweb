08792f01d4c8c1fce194467eae068557275605b2
andreLagacione <lagacioneandre@gmail.com>
2020-02-13 22:28:23 -0300

    print formatted xml

--- src/components/upload-document/show-xml/show-xml.js
+++ src/components/upload-document/show-xml/show-xml.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import beautify from 'xml-beautifier';
+import './show-xml.scss';
 
-function ShowXml({xml}) {
+function ShowXml({ xml }) {
 
+    const [xmlEnviado, setXmlEnviado] = useState([]);
+
+    useEffect(() => {
+        if (!xml instanceof Array) {
+            console.log('vaca')
+            setXmlEnviado(beautify(xml));
+        }
+    }, []);
 
     return (
         <>
             <div className="xml-document">
                 <h3 className="title">Documento enviado</h3>
 
-                <div className="content">{xml}</div>
+                <div className="content">{xmlEnviado}</div>
             </div>
         </>
     )
