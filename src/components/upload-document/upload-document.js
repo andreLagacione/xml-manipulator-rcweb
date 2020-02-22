dfa27567609eace1c110e3ffdda4ed5eb93d57dd
andreLagacione <lagacioneandre@gmail.com>
2020-02-22 16:28:47 -0300

    add option to inform custom tag

--- src/components/upload-document/upload-document.js
+++ src/components/upload-document/upload-document.js
@@ -7,6 +7,7 @@ import './upload-document.scss';
 import { sendFileRequest } from './service/upload-document-service';
 import ShowXml from '../show-xml/show-xml';
 import AlertModel from '../../commons/alert-modal/alert-modal';
+import removeSpecialChars from '../../commons/formatter/remove-special-chars';
 
 function UploadDocument() {
 
@@ -15,17 +16,19 @@ function UploadDocument() {
     const [xmlEnviado, setXmlEnviado] = useState([]);
     const [showUploadResponse, setShowUploadResponse] = useState([]);
     const [configModal, setConfigModal] = useState([]);
+    let tagNameValue;
+    let tagValueValue;
 
     let fileInputRef = React.createRef();
 
-    function fileAdded(event) {
+    const fileAdded = (event) => {
         let fileInput = event.target.files[0];
         let _filename = fileInput.name;
         setFile(fileInput);
         setFileName(_filename);
     }
 
-    async function sendFile(event) {
+    const sendFile = async (event) =>  {
         event.preventDefault();
 
         if (file instanceof Array && !file.length) {
@@ -37,8 +40,31 @@ function UploadDocument() {
             return false;
         }
 
-        const _response = await sendFileRequest('/document', file);
+        const tagName = tagNameValue.value;
+        const tagValue = tagValueValue.value;
 
+        if (tagName.length && tagValue.length) {
+            addNewEditedFile(tagName, tagValue);
+            return false;
+        }
+
+        addNewFile();
+        
+    }
+
+    const addNewFile = async () => {
+        mapResponse(await sendFileRequest('/document', file));
+    }
+
+    const addNewEditedFile = async (tagName, tagValue) => {
+        const bodyParams = {
+            tagName: tagName,
+            tagValue: tagValue
+        }
+        mapResponse(await sendFileRequest('/edited-document/create', file, bodyParams));
+    }
+
+    const mapResponse = (_response) => {
         if (_response.status === 200) {
             setXmlEnviado(_response.data.content);
             setShowUploadResponse(true);
@@ -53,10 +79,14 @@ function UploadDocument() {
         });
     }, []);
 
-    function handleOnCloseModal() {
+    const handleOnCloseModal = () => {
         setConfigModal({
             show: false
         });
+    }
+
+    const buildName = () => {
+        tagNameValue.value = removeSpecialChars(tagNameValue.value);
     }
 
     return (
@@ -64,14 +94,43 @@ function UploadDocument() {
             <form onSubmit={sendFile} id="document-upload">
                 <input type="file" name="fileInput" id="fileInput" accept=".xml" ref={fileInputRef} hidden onChange={fileAdded} />
 
-
                 <label htmlFor="fileInput" className="file-name">
                     {fileName}
                     <span className="btn-select">Selecione</span>
                 </label>
 
+                <code className="alert-text">* O documento tem que estar dentro das tags "&lt;xml&gt;&lt;/xml&gt;"</code>
+
+                <div className={'row-add-tag ' + (!fileName.length ? 'hidden' : '')}>
+                    <div className="box-input">
+                        <label htmlFor="tagName">Nome da tag</label>
+                        <input
+                            type="text"
+                            id="tagNameRef"
+                            name="tagName"
+                            placeholder="ex: pontoReferencia"
+                            ref={(tagName) => tagNameValue = tagName}
+                            onBlur={buildName}
+                        />
+                    </div>
+
+                    <div className="box-input">
+                        <label htmlFor="tagValue">Valor da tag</label>
+                        <input
+                            type="text"
+                            id="tagValueRef"
+                            name="tagValue"
+                            ref={(tagValue) => tagValueValue = tagValue}
+                            onBlur={buildName} />
+                    </div>
+                </div>
+
+                <code className={'alert-text ' + (!fileName.length ? 'hidden' : '')}>
+                    Se desejar você pode adicionar uma tag para o destinatário,
+                    o nome dessa tag não pode conter caractéres especias e/ou espaço em branco.
+                </code>
+
                 <div className="row-button">
-                    <code className="alert-text">* O documento tem que estar dentro das tags "&lt;xml&gt;&lt;/xml&gt;"</code>
                     <button type="submit" className="btn-submit">
                         <FontAwesomeIcon icon={faUpload} /> Enviar
                     </button>
