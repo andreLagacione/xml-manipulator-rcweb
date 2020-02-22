2049b11c3f5226055a9722f3e68478064c729363
andreLagacione <lagacioneandre@gmail.com>
2020-02-22 16:29:06 -0300

    add params for custom tag

--- src/components/upload-document/service/upload-document-service.js
+++ src/components/upload-document/service/upload-document-service.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
-export const sendFileRequest = (url, file) => {
+export const sendFileRequest = (url, file, bodyParams) => {
     const formData = new FormData();
+    formData.append('file', file);
 
-    formData.append('file', file)
+    if (bodyParams) {
+        formData.append('tagName', bodyParams.tagName);
+        formData.append('tagValue', bodyParams.tagValue);
+    }
 
     return axios.post(url, formData, {
         headers: {
